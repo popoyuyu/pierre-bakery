30e98eeeb2f11384045afe08b0b77286b4ca7b41
Jeff Lai <pjefflai@gmail.com>
2021-12-10 10:16:25 -0800

    Change constructor setup

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -11,7 +11,7 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
   }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,11 +4,12 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    // public int BreadCost { get; set; }
-    // public static int BreadTotal(int cost)
-    // {
-    //   Bread breadPrice = new Bread();
-    //   breadPrice.BreadCost = 5;
-    // }
+    public int BreadCost { get; set; }
+    public int BreadAmount { get; set; }
+    public Bread(int breadAmount)
+    {
+      BreadAmount = breadAmount;
+      BreadCost = 0;
+    }
   }
 }
