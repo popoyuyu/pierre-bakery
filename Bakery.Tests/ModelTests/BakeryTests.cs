736211490f6ba5e3008b6b0b72bed73febd9596c
Jeff Lai <pjefflai@gmail.com>
2021-12-10 08:37:09 -0800

    add first test to create new instance of class Bread

---
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class Bread
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = newBread();
+      AssemblyLoadEventArgs.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
